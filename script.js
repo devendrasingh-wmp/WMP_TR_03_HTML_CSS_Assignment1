902f70f1605e566cce7158eb40434ca6bdbc2f86
devendra <devendra5141697@gmail.com>
2024-10-23 10:30:48 +0530

    All files are added

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-btn').addEventListener('click', function() {
+  const navCollapse = document.getElementById('navCollapse');
+  navCollapse.classList.toggle('active');
+});
